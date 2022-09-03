91c8fc08616bf896821b105442febd19b5c0d2ce
Семён Новиков <simon.novikov@gmail.com>
2022-09-04 01:07:48 +0300

    Pre Finish commit Check out Errors

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
 
 
 def index(request):
@@ -6,26 +7,24 @@ def index(request):
     template = 'posts/index.html'
 
     title = 'Yatube'
-
     text = 'Это главная страница проекта Yatube'
+    posts = Post.objects.order_by('-pub_date')[:10]
+
     context = {
         'title': title,
         'text': text,
+        'posts': posts,
     }
 
     return render(request, template, context)
-
 
 def group_posts(request, slug):
 
-    template = 'posts/group_list.html'
-
-    title = 'Yatube'
-
-    text = 'Здесь будет информация о группах проекта Yatube'
+    group = get_object_or_404(Group, slug=slug)
+    
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     context = {
-        'title': title,
-        'text': text,
+        'group': group,
+        'posts': posts,
     }
-    # Третьим параметром передаём словарь context
-    return render(request, template, context)
+    return render(request, 'posts/group_list.html', context) 
