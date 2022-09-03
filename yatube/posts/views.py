9cfaafed1973f397b5736f68e82666694a22de44
Семён Новиков <simon.novikov@gmail.com>
2022-09-03 23:02:39 +0300

    bootstrap added

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -2,10 +2,30 @@ from django.shortcuts import render
 
 
 def index(request):
+
     template = 'posts/index.html'
-    return render(request, template)
+
+    title = 'Yatube'
+
+    text = 'Это главная страница проекта Yatube'
+    context = {
+        'title': title,
+        'text': text,
+    }
+
+    return render(request, template, context)
 
 
 def group_posts(request, slug):
+
     template = 'posts/group_list.html'
-    return render(request, template)
+
+    title = 'Yatube'
+
+    text = 'Здесь будет информация о группах проекта Yatube'
+    context = {
+        'title': title,
+        'text': text,
+    }
+    # Третьим параметром передаём словарь context
+    return render(request, template, context)
